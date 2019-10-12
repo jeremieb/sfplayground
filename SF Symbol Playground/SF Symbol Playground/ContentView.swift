27fe288ed1096827f92d18316c7449f32866ddd4
Jeremie Berduck <jeremieberduck@gmail.com>
2019-10-12 11:03:17 +0200

    Initial commit
    App description and capabilities. Added support for iPad and Mac OS... (fuck yeah!)

---
+++ SF Symbol Playground/SF Symbol Playground/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SF Symbol Playground
+//
+//  Created by Jeremie Berduck on 12/10/2019.
+//  Copyright © 2019 Jeremie Berduck. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
